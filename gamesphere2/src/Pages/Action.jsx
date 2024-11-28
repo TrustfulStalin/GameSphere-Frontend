afd3e5366edf074aab7ec045c240ec0ec78be2cd
Tevon Talbert <TevonTalbert@hotmail.com>
2024-11-28 00:50:39 -0600

    heroku change1

--- gamesphere2/src/Pages/Action.jsx
+++ gamesphere2/src/Pages/Action.jsx
@@ -12,7 +12,7 @@ const ActionCreate = () => {
 
   const fetchData = async () => {
     try {
-      const response = await fetch(`https://capstone-back-78a0aa10b637.herokuapp.com/actions`);
+      const response = await fetch(`https://backgame-c41e5170a8e2.herokuapp.com//actions`);
       if (!response.ok) {
         throw new Error('Failed to fetch data');
       }
@@ -25,7 +25,7 @@ const ActionCreate = () => {
 
   const handleDelete = async (id) => {
     try {
-      const response = await fetch(`https://capstone-back-78a0aa10b637.herokuapp.com/actions/${id}`, {
+      const response = await fetch(`https://backgame-c41e5170a8e2.herokuapp.com//actions/${id}`, {
         method: 'DELETE'
       });
       if (!response.ok) {
